3e3f2f125d96d62d26003e6d878a59b7250221de
Akim Minko <akim.minko@yandex.ru>
2024-02-21 22:39:27 +0300

    refactor: position sale button

--- js/index.js
+++ js/index.js
@@ -1,34 +1,26 @@
 function burgerMenu(selector) {
-  
   let menu = document.querySelector(selector);
   let button = menu.find(".burger__menu-button");
   let links = menu.find(".burger__menu-link");
   let overlay = menu.find(".burger__menu-overlay");
 
   button.addEventListener("click", (e) => {
-    
     e.preventDefault();
     toggleMenu();
-  }
-  );
+  });
 
   links.addEventListener("click", () => toggleMenu());
 
   overlay.addEventListener("click", () => toggleMenu());
 
   function toggleMenu() {
-    
-    menu.classList.toggle('burger__menu_active');
-    // if(menu.hasClass('burger__menu_active')) {
-      
-    //   document.querySelector('body').style.'overflow', 'hidden')
-    // }
-    //  else {
-      
-    //   document.querySelector('body').style.'overflow', 'visible')
-    // }
+    menu.classList.toggle("burger__menu_active");
+    if (menu.classList.contains("burger__menu_active")) {
+      document.querySelector("body").style.overflow = "hidden";
+    } else {
+      document.querySelector("body").style.overflow = "visible";
+    }
   }
 }
-
 
 burgerMenu(".burger__menu");
